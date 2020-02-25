c5ef30f0b5262ef73a3a1429f44409d9d49bb5b8
alexanderK1987 <tainancgw@gmail.com>
2020-02-25 17:21:24 +0800

    add os level relay

---
+++ os_relay.py
@@ -0,0 +1,122 @@
+import re
+from pymodbus.client.sync import ModbusTcpClient as MbClient
+import threading
+import time
+import rostopic
+import subprocess
+
+NODE_NAME = 'ROS_MODBUS_RELAY'
+PUB_TOPIC = 'modbus_tcp_data'
+SUB_TOPIC = 'modbus_tcp_cmd'
+CMD_PATTERN = '(READ|WRITE)\s+(\w+)\s+(COUNT)\s+(\w+)(\s*[\w|\W]*){0,1}'
+MODBUS_HOST = '192.168.10.30'
+MODBUS_PORT = 502
+MODBUS_TIMEOUT = 7e-3
+
+class msg_handler():
+    def __init__(self, client):
+        self.client = client
+        self.proc = subprocess.Popen(['rostopic', 'echo', '/'+SUB_TOPIC], stdout=subprocess.PIPE)
+        self.prog = re.compile(CMD_PATTERN)
+        self.t_thread = threading.Thread(target=self.start)
+        self.kill = False
+        self.t_thread.setDaemon(True)
+        self.t_thread.start()
+
+    def start(self):
+        while self.kill == False:
+            data = self.proc.stdout.readline()
+            if data.startswith("data:"):
+                self.process(data.split('"')[1])
+
+    def process(self, line):
+        print (line)
+        m = self.prog.match(line)
+        if m == None:
+            print ('invalid command %s' % line)
+            return
+        cmd, addr, count = m.group(1), eval(m.group(2)), int(m.group(4))
+        if cmd == 'WRITE':
+            values = map(eval, m.group(5).split())
+            values = [v&0x0000ffff for v in values]
+            if (values[1] == 0) and (values[2] == 0):
+                values[0] = 2
+            if len(values) != count:
+                print ('incorret amount of data')
+                return
+            if not self.client.is_socket_open():
+                self.client.connect()
+            self.async_write(addr, values)
+            return
+            
+    def async_write(self, addr, count, unit=1):
+        if not self.client.is_socket_open():
+            return 
+        t = threading.Thread(target=self.__async_write__, args=(addr,count,), kwargs={'unit':unit})
+        t.setDaemon(True)
+        t.start()
+
+    def __async_write__(self, addr, values, unit=1):
+        try:
+            self.client.write_registers(addr, values, unit=unit)
+        except:
+            pass
+
+class HbChecker():
+    def __init__(self):
+        self.TIMEOUT = 1.0 # seconds
+        self.kill = False
+        self.ros_alive = False
+        self.t_checker = threading.Thread(target=self.__start__)
+        self.t_checker.setDaemon(True)
+        self.t_checker.start()
+        print ('start checking HB')
+
+    def __start__(self):
+        while not self.kill:
+            try:
+                time.sleep(self.TIMEOUT)
+                topic = rostopic.get_topic_class('/modbus_tcp_cmd')
+                if topic[0] == None:
+                    self.ros_alive = False
+                else:
+                    self.ros_alive = True
+            except:
+                self.ros_alive = False
+    
+if __name__  == '__main__':
+    handler = None
+    client = MbClient(MODBUS_HOST, port=MODBUS_PORT, timeout=MODBUS_TIMEOUT)
+    client.connect()
+    hbc = HbChecker()
+    KILL = False
+    mh = None
+    while not KILL:
+        try:
+            while hbc.ros_alive == False and (KILL == False):
+                print ('reconnecting')
+                if mh != None:
+                    mh.kill = True
+
+                time.sleep(hbc.TIMEOUT)
+        except KeyboardInterrupt:
+            print ('SIGTERM.')
+            hbc.kill = True
+            KILL = True
+        if not KILL:
+            mh = msg_handler(client)
+        try:
+            while hbc.ros_alive and (KILL == False):
+                time.sleep(hbc.TIMEOUT)
+
+        except KeyboardInterrupt:
+            print ('SIGTERM.')
+            hbc.kill = True
+            KILL = True
+            break
+
+    print ('Terminating')
+    if mh != None:
+        mh.kill = True
+    client.close()
+
